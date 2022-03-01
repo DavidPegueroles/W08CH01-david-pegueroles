66f9dda59340bf611a07d2ead57a4022682dbcc6
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-01 22:01:26 +0100

    Added ssr page in mypokemon folder

---
+++ pages/mypokemon/ssr.js
@@ -0,0 +1,40 @@
+import styles from "../../styles/PokemonPages.module.css";
+import Image from "next/image";
+
+const PokemonSSR = ({ pokemonList }) => {
+  return (
+    <>
+      <h1 className={styles.title}>Pokémon List SSR</h1>
+      <ul className={styles.pokemonList}>
+        {pokemonList.map((pokemon) => (
+          <li key={pokemon.id} className={styles.pokemonListItemClickable}>
+            <div className={styles.pokemonImageContainer}>
+              <Image
+                src={pokemon.sprites.other["official-artwork"].front_default}
+                alt={pokemon.name}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+            <p className={styles.pokemonText}>
+              {pokemon.forms[0].name.toUpperCase()}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const response = await fetch(process.env.NEXT_PUBLIC_OUR_POKE_API);
+  const pokemonList = await response.json();
+
+  return {
+    props: {
+      pokemonList,
+    },
+  };
+};
+
+export default PokemonSSR;
