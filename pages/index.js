87a8340ad0508ba61db77ef0cf4ea443c499d0e2
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-03-02 03:53:20 +0100

    Added a custom font-family to the text

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,18 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Pokémon</h1>
+
+        <p className={styles.description}>
+          In this site you can check different ways of rendering a page in
+          Next.js.
+        </p>
+
+        <p className={styles.description}>
+          Use the navigation menu in the top to get to different pages and check
+          the Network window in the Dev Tools to see the differences.
+        </p>
+
+        <p className={styles.description}>Have fun!</p>
       </main>
     </div>
   );
